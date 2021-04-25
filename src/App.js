06ca88175ed355e45732c3663475b15cfe8d46c7
User <shevkar.sanket@gmail.com>
2021-04-25 08:42:47 +0530

    create room added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import firebase from './firebase/index'
 
 const configuration = {
@@ -14,13 +14,14 @@ const configuration = {
     iceCandidatePoolSize: 10,
   };
 
+let peerConnection = null;
+
 function App() {
     
   const [media, setMedia] = useState(false);
   const [createRoom, setCreateRoom] = useState(true);
   const [joinRoom, setJoinRoom] = useState(true);
   const [hangUp, setHangUp] = useState(true);
-  const [peerConnection, setPeerConnection] = useState(null);
   const [roomId, setRoomId] = useState(null);
 
   const localStream = useRef();
@@ -48,47 +49,76 @@ function App() {
   }
 
   const onClickCreateRoom = async() =>{
-//     console.log(createRoom);
-//     useEffect(() => { setCreateRoom(false) }, [])
-//     setJoinRoom(false);
-//     const db = firebase.firestore();
-//     const roomRef = await db.collection('rooms').doc();
-//     console.log('Create PeerConnection with configuration: ', configuration);
-//     const newPeerConnection = new RTCPeerConnection(configuration);
-//     console.log(newPeerConnection);
-//     setPeerConnection(newPeerConnection);
-//     console.log(peerConnection);   
-//     registerPeerConnectionListeners();
-//     localStream.current.srcObject.getTracks().forEach(track => {
-//         peerConnection.addTrack(track, localStream);
-//       });
-//     // Code for collecting ICE candidates below
-//     const callerCandidatesCollection = roomRef.collection('callerCandidates');
+    console.log(createRoom);
+    setCreateRoom(false);
+    setJoinRoom(false);
+    const db = firebase.firestore();
+    const roomRef = await db.collection('rooms').doc();
+    console.log('Create PeerConnection with configuration: ', configuration);
+    peerConnection = new RTCPeerConnection(configuration); 
+    registerPeerConnectionListeners();
+    localStream.current.srcObject.getTracks().forEach(track => {
+        peerConnection.addTrack(track, localStream.current.srcObject);
+      });
+    // Code for collecting ICE candidates below
+    const callerCandidatesCollection = roomRef.collection('callerCandidates');
 
-//     peerConnection.addEventListener('icecandidate', event => {
-//         if (!event.candidate) {
-//           console.log('Got final candidate!');
-//           return;
-//         }
-//         console.log('Got candidate: ', event.candidate);
-//         callerCandidatesCollection.add(event.candidate.toJSON());
-//       });
-//     // Code for collecting ICE candidates above
+    peerConnection.addEventListener('icecandidate', event => {
+        if (!event.candidate) {
+          console.log('Got final candidate!');
+          return;
+        }
+        console.log('Got candidate: ', event.candidate);
+        callerCandidatesCollection.add(event.candidate.toJSON());
+      });
+    // Code for collecting ICE candidates above
 
-//     // Code for creating a room below
-//     const offer = await peerConnection.createOffer();
-//     await peerConnection.setLocalDescription(offer);
-//     console.log('Created offer:', offer);
+    // Code for creating a room below
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    console.log('Created offer:', offer);
 
-//     const roomWithOffer = {
-//         'offer': {
-//         type: offer.type,
-//         sdp: offer.sdp,
-//         },
-//     };
-//     await roomRef.set(roomWithOffer);
-//     setRoomId(roomRef.id);
-//     console.log(`New room created with SDP offer. Room ID: ${roomRef.id}`);
+    const roomWithOffer = {
+        'offer': {
+        type: offer.type,
+        sdp: offer.sdp,
+        },
+    };
+    await roomRef.set(roomWithOffer);
+    setRoomId(roomRef.id);
+    // Code for creating a room above
+
+    peerConnection.addEventListener('track', event => {
+      console.log('Got remote track:', event.streams[0]);
+      event.streams[0].getTracks().forEach(track => {
+        console.log('Add a track to the remoteStream:', track);
+        remoteStream.current.srcObject.addTrack(track);
+      });
+    });
+
+    // Listening for remote session description below
+    roomRef.onSnapshot(async snapshot => {
+      const data = snapshot.data();
+      if (!peerConnection.currentRemoteDescription && data && data.answer) {
+        console.log('Got remote description: ', data.answer);
+        const rtcSessionDescription = new RTCSessionDescription(data.answer);
+        await peerConnection.setRemoteDescription(rtcSessionDescription);
+      }
+    });
+    // Listening for remote session description above
+
+    // Listen for remote ICE candidates below
+    roomRef.collection('calleeCandidates').onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(async change => {
+        if (change.type === 'added') {
+          let data = change.doc.data();
+          console.log(`Got new remote ICE candidate: ${JSON.stringify(data)}`);
+          await peerConnection.addIceCandidate(new RTCIceCandidate(data));
+        }
+      });
+    });
+    // Listen for remote ICE candidates above
+
   }
 
   const onClickMedia = async() =>{
